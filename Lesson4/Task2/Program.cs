393f9dded52e213ba584aafa431d317964d63014
EastWally <z.anastacia.a@gmail.com>
2021-01-31 19:19:04 +0300

    Практическое задание к уроку 4

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите набор чисел, разделённых пробелом:");
+            string[] inputString = Console.ReadLine().Replace('.',',').Split(' ');
+            double sum = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (double.TryParse(inputString[i], out double number))
+                    sum += number;
+            }
+            Console.WriteLine($"Сумма введёных чисел равна {sum}");
+            Console.ReadKey();
+        }
+    }
+}
