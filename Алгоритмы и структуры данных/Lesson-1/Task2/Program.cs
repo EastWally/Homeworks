80c3ce5cf3a384acc1eab20b524c7c5e2cc64f65
EastWally <z.anastacia.a@gmail.com>
2021-02-22 17:22:54 +0300

    Практическое задание к уроку 1

---
+++ Алгоритмы и структуры данных/Lesson-1/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Асимптотическая  сложность функции O(N)*O(N)*O(N) = O(N^3)
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)            //O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++)        //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)    //O(N)
+                    {
+                        int y = 0;                                                           
+
+                        if (j != 0)                                
+                        {
+                            y = k / j;                                         
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;      
+                    }
+                }
+            }
+
+            return sum;
+            
+        }
+    }
+}
