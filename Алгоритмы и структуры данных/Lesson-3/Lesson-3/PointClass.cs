425e6333ae1be32c395264f4abdeb89387f296eb
EastWally <z.anastacia.a@gmail.com>
2021-03-03 22:04:43 +0300

    Практическое задание к уроку 3

---
+++ Алгоритмы и структуры данных/Lesson-3/Lesson-3/PointClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_3
+{
+    public class PointClass
+    {
+        public int X;
+        public int Y;
+
+        public static float PointDistance(PointClass pointOne, PointClass pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+    }
+}
---
+++ Алгоритмы и структуры данных/Lesson-3/Lesson-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Lesson_3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadKey();
+        }
+    }
+    public class BechmarkClass
+    {
+        PointClass pointClassOne = new PointClass() { X = 10, Y = 30 };
+        PointClass pointClassTwo = new PointClass() { X = 5, Y = 90 };
+        PointStruct pointStructOne = new PointStruct() { X = 10, Y = 30 };
+        PointStruct pointStructTwo = new PointStruct() { X = 5, Y = 90 };
+
+
+        [Benchmark]
+        public void Test_PointDistance_ClassFloat()
+        {
+            PointClass.PointDistance(pointClassOne, pointClassTwo);
+        }
+
+        [Benchmark]
+        public void Test_PointDistance_StructFloat()
+        {
+            PointStruct.PointDistance(pointStructOne, pointStructTwo);
+        }
+
+        [Benchmark]
+        public void Test_PointDistance_StructDouble()
+        {
+            PointStruct.PointDistanceDouble(pointStructOne, pointStructTwo);
+        }
+
+        [Benchmark]
+        public void Test_PointDistance_StructShort()
+        {
+            PointStruct.PointDistanceShort(pointStructOne, pointStructTwo);
+        }
+
+    }
+}
