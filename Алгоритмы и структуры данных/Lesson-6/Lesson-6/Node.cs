6266d25e2e6a1eba21fe326a566fc5f432f01865
EastWally <z.anastacia.a@gmail.com>
2021-03-14 17:39:09 +0300

    Практическое задание к уроку 6

---
+++ Алгоритмы и структуры данных/Lesson-6/Lesson-6/Node.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_6
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public List<Edge> Edges { get; set; }
+        public bool visited = false;
+
+        public Node()
+        {
+            Edges = new List<Edge>();
+        }
+
+        public static void DFS(Node startNode)
+        {
+            startNode.visited = true;
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(startNode);
+            while (stack.Count != 0)
+            {
+                Node node = stack.Pop();
+                Console.Write($" [{node.Value}] ");
+                foreach (var link in node.Edges)
+                {
+                    if (!link.Node.visited)
+                    {
+                        link.Node.visited = true;
+                        stack.Push(link.Node);
+                    }
+                }
+            }
+            ClearVisited(startNode);
+        }
+
+        public static void BFS(Node startNode)
+        {
+            startNode.visited = true;
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(startNode);
+            while (queue.Count != 0)
+            {
+                Node node = queue.Dequeue();
+                Console.Write($" [{node.Value}] ");
+                foreach (var link in node.Edges)
+                {
+                    if (!link.Node.visited)
+                    {
+                        link.Node.visited = true;
+                        queue.Enqueue(link.Node);
+                    }
+                }
+            }
+            ClearVisited(startNode);
+        }
+
+        //Очистка признака посещения узла в графе
+        static void ClearVisited(Node startNode)
+        {
+            startNode.visited = false;
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(startNode);
+            while (stack.Count != 0)
+            {
+                Node node = stack.Pop();
+                foreach (var link in node.Edges)
+                {
+                    if (link.Node.visited)
+                    {
+                        link.Node.visited = false;
+                        stack.Push(link.Node);
+                    }
+                }
+            }
+        }
+    }    
+}
---
+++ Алгоритмы и структуры данных/Lesson-6/Lesson-6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = GenerateGraph();
+            Console.Write("DFS: ");
+            Node.DFS(node);
+            Console.Write("\nBFS: ");
+            Node.BFS(node);
+            Console.ReadKey();
+        }
+
+        static Node GenerateGraph()
+        {
+            Node node1 = new Node() { Value = 1 };
+            Node node2 = new Node() { Value = 2 };
+            Node node3 = new Node() { Value = 3 };
+            Node node4 = new Node() { Value = 4 };
+            Node node5 = new Node() { Value = 5 };
+            Node node6 = new Node() { Value = 6 };
+            node1.Edges.Add(new Edge() { Node = node5, Weight = 1 });
+            node1.Edges.Add(new Edge() { Node = node2, Weight = 1 });  
+            node2.Edges.Add(new Edge() { Node = node6, Weight = 1 });
+            node2.Edges.Add(new Edge() { Node = node3, Weight = 1 });
+            node2.Edges.Add(new Edge() { Node = node1, Weight = 1 });
+            node3.Edges.Add(new Edge() { Node = node6, Weight = 1 });
+            node3.Edges.Add(new Edge() { Node = node4, Weight = 1 });
+            node3.Edges.Add(new Edge() { Node = node2, Weight = 1 });
+            node4.Edges.Add(new Edge() { Node = node5, Weight = 1 });
+            node4.Edges.Add(new Edge() { Node = node3, Weight = 1 });
+            node5.Edges.Add(new Edge() { Node = node6, Weight = 1 });
+            node5.Edges.Add(new Edge() { Node = node4, Weight = 1 });
+            node5.Edges.Add(new Edge() { Node = node1, Weight = 1 });
+            node6.Edges.Add(new Edge() { Node = node5, Weight = 1 });
+            node6.Edges.Add(new Edge() { Node = node3, Weight = 1 });
+            node6.Edges.Add(new Edge() { Node = node2, Weight = 1 });
+            return node1;
+        }
+    }
+}
