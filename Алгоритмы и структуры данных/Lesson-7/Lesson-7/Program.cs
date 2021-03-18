33933557c2659fab20c60baf41febf8c26623d84
EastWally <z.anastacia.a@gmail.com>
2021-03-18 19:32:44 +0300

    Практическое задание к уроку 7

---
+++ Алгоритмы и структуры данных/Lesson-7/Lesson-7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 64;
+            int M = 100;
+            int countPath = CountPath(M, N);            
+            Console.WriteLine(countPath);
+            
+            Console.ReadKey();
+        }
+
+        public static int CountPath(int M, int N)
+        {
+            if (M <= 0 || N <= 0)
+                throw new ArgumentException();
+            
+            if (M == 1)
+                return 1;
+            if (N == 1)
+                return 1;
+
+            return CountPath(M, N - 1) + CountPath(M - 1, N);
+        }
+    }    
+}
